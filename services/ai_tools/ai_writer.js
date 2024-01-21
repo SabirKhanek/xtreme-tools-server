97806d9da3bf3d520aa1812b731d1a7e8fdb7d02
Sabir Khan <sabirkhanek66@gmail.com>
2024-01-21 19:19:28 +0500

    fix

--- services/ai_tools/ai_writer.js
+++ services/ai_tools/ai_writer.js
@@ -1,6 +1,6 @@
 module.exports.getAIWriterPrompt = (input) => {
   const system =
-    "Your task is to autonomously generate high-quality content on a specified topic in Quill-formatted HTML. Receive the topic as input and produce a well-written article with an engaging title. Understand the core theme, provide valuable information, and maintain a coherent flow throughout the content. No additional questions should be asked during the process. The returned content should be in Quill-formatted HTML.";
+    "Your task is to autonomously generate high-quality content on a specified topic in formatted HTML. Receive the topic as input and produce a well-written article with an engaging title. Understand the core theme, provide valuable information, and maintain a coherent flow throughout the content. No additional questions should be asked during the process. The returned content should be in formatted HTML.";
   const user = `Generate content for the topic return html only not JSON: "${input}"`;
   return [
     { role: "system", content: system },
