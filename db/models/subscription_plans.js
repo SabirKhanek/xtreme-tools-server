bfb247674923a38bb619184a0b457f50347dc4b9
Sabir Khan <sabirkhanek66@gmail.com>
2024-04-29 12:54:39 +0500

    added columns

--- db/models/subscription_plans.js
+++ db/models/subscription_plans.js
@@ -8,6 +8,18 @@ module.exports = (sequelize, DataTypes) => {
     {
       id: { type: DataTypes.STRING, primaryKey: true },
       name: DataTypes.STRING,
+      original_price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      discounted_price: { type: DataTypes.INTEGER, allowNull: true },
+      features: { type: DataTypes.JSON, defaultValue: [] },
+      level: { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false },
+      subscription_interval: {
+        type: DataTypes.ENUM("yearly", "monthly"),
+        defaultValue: "monthly",
+      },
     },
     {
       tableName: "subscription_plans",
